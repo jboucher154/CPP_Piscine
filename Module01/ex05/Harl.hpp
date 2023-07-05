c314a29d633449d73851aabaa3505d457080d4d6
Jenny Boucher <jebouche@c3r4p2.hive.fi>
2023-07-05 11:39:35 +0300

    add for loop for printing and test functions to main

--- Module01/ex05/Harl.hpp
+++ Module01/ex05/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: jebouche <jebouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 17:29:51 by jebouche          #+#    #+#             */
-/*   Updated: 2023/07/04 17:40:40 by jebouche         ###   ########.fr       */
+/*   Updated: 2023/07/05 11:38:09 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,14 @@
 # define HARL_HPP
 
 # include <string>
+
+# define COLOR_RESET "\033[0m"
+# define COLOR_BRIGHT_RED "\033[91m"
+# define COLOR_BRIGHT_GREEN "\033[92m"
+# define COLOR_BRIGHT_YELLOW "\033[93m"
+# define COLOR_BRIGHT_CYAN "\033[96m"
+# define COLOR_BRIGHT_MAGENTA "\033[95m"
+# define BGCOLOR_PURPLE_BLUE "\033[1;36;45m"
 
 class Harl
 {
--- Module01/ex05/main.cpp
+++ Module01/ex05/main.cpp
@@ -1,11 +1,45 @@
 
 #include "Harl.hpp"
+#include <iostream>
+#include <ctime>
+
+void	many_complaints( void ) {
+	Harl		harl_one;
+	std::string	levels[] = { "DEBUG", "INFO", "WARNING", "ERROR"};
+
+	srand(time(NULL));
+	std::cout << BGCOLOR_PURPLE_BLUE << "Testing many complaints" << COLOR_RESET << std::endl;
+	for (int i = 0; i < 100; i++) {
+		harl_one.complain(levels[std::rand() % 4]);
+	}
+	std::cout << BGCOLOR_PURPLE_BLUE << "Completed" << COLOR_RESET << std::endl;
+}
+
+void	simple_tests( void ) {
+	Harl	harl_one;
+
+	std::cout << BGCOLOR_PURPLE_BLUE << "Testing each complaint" << COLOR_RESET << std::endl;
+	harl_one.complain("DEBUG");
+	harl_one.complain("INFO");
+	harl_one.complain("WARNING");
+	harl_one.complain("ERROR");
+	std::cout << BGCOLOR_PURPLE_BLUE << "Completed" << COLOR_RESET << std::endl;
+}
+
+void	input_tests( void ) {
+	Harl	harl_one;
+
+	std::cout << BGCOLOR_PURPLE_BLUE << "Testing bad input" << COLOR_RESET << std::endl;
+	harl_one.complain("");
+	harl_one.complain("nomatch");
+	harl_one.complain("DEBUGINFO");
+	std::cout << BGCOLOR_PURPLE_BLUE << "Completed" << COLOR_RESET << std::endl;
+}
 
 int	main( void ) {
 	
-	Harl	harl_one;
-
-	harl_one.complain("DEBUG");
-
+	input_tests();
+	simple_tests();
+	many_complaints();
 	return (0);
 }
