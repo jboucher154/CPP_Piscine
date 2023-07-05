7f786f1698f3f03f7bb66431fced4bf2d4c261e1
Jenny Boucher <jebouche@c3r4p2.hive.fi>
2023-07-05 16:22:54 +0300

    add newlines, case for noe weapon for B, obj file compilation for makefile

--- Module01/ex03/HumanA.cpp
+++ Module01/ex03/HumanA.cpp
@@ -6,7 +6,7 @@
 /*   By: jebouche <jebouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 15:45:44 by jebouche          #+#    #+#             */
-/*   Updated: 2023/07/05 15:45:45 by jebouche         ###   ########.fr       */
+/*   Updated: 2023/07/05 16:19:16 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,5 @@ HumanA::~HumanA() {}
 
 void	HumanA::attack( void ) const {
 	std::string type = this->_weapon.getType();
-	std::cout << this->_name << " attacks with their " << type;
+	std::cout << this->_name << " attacks with their " << type << std::endl;
 }
--- Module01/ex03/HumanB.cpp
+++ Module01/ex03/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: jebouche <jebouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 15:45:53 by jebouche          #+#    #+#             */
-/*   Updated: 2023/07/05 15:45:54 by jebouche         ###   ########.fr       */
+/*   Updated: 2023/07/05 16:19:06 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@ HumanB::HumanB( std::string name ) : _name(name), _weapon(NULL) {}
 HumanB::~HumanB() {}
 
 void	HumanB::attack( void ) const {
-	std::string type = this->_weapon->getType();
-	std::cout << this->_name << " attacks with their " << type;
+	if (this->_weapon == NULL)
+		std::cout << this->_name << " attacks with nothing" << std::endl;
+	else
+		std::cout << this->_name << " attacks with their " << this->_weapon->getType() << std::endl;
 }
 
 void	HumanB::setWeapon( Weapon new_weapon) {
