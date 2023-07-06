6f84993ea6065cee700441b10a28fa2d3a1bbccf
Jenny Boucher <jebouche@c3r3p3.hive.fi>
2023-07-06 16:10:14 +0300

    add content for mod2 ex00

---
+++ Module02/ex00/Fixed.cpp
@@ -0,0 +1,39 @@
+
+#include "Fixed.hpp"
+#include <iostream>
+
+const int Fixed::_fractionlBits = 8;
+
+Fixed::Fixed( void ) : _fixed_point_num(0) {
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed( int fpNum ) : _fixed_point_num(fpNum) {
+	std::cout << "Paramaterized constructor called" << std::endl;
+}
+
+//could use the assignment operator call here
+Fixed::Fixed( Fixed& to_copy ) {
+	std::cout << "Copy constructor called" << std::endl;
+	this->_fixed_point_num = to_copy.getRawBits();
+}
+
+Fixed::~Fixed( void ) {
+	std::cout << "Deconstructor called" << std::endl;
+}
+
+Fixed& Fixed::operator=( Fixed& to_copy) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->_fixed_point_num = to_copy.getRawBits();
+	return (*this);
+}
+
+int		Fixed::getRawBits( void ) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_fixed_point_num);
+}
+
+void	Fixed::setRawBits( int const raw ) {
+	std::cout << "setRawbits member function called" << std::endl;
+	this->_fixed_point_num = raw;
+}
---
+++ Module02/ex00/Fixed.hpp
@@ -0,0 +1,22 @@
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+class Fixed
+{
+	public:
+		Fixed( void );
+		Fixed( int fpNum ); 
+		Fixed( Fixed& to_copy );
+		~Fixed( void );
+		Fixed &operator=( Fixed& to_copy);
+
+		int		getRawBits( void ) const;
+		void	setRawBits( int const raw );
+
+	private:
+		static const int	_fractionlBits;
+		int					_fixed_point_num;
+};
+
+#endif
