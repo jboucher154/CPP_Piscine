17015527796d088e16cffa88348d7b52ebe601e6
Jenny Boucher <jebouche@c3r4p5.hive.fi>
2023-07-11 15:38:12 +0300

    add test code from the pdf

--- Module02/ex01/main.cpp
+++ Module02/ex01/main.cpp
@@ -2,42 +2,21 @@
 #include <iostream>
 #include "Fixed.hpp"
 
-#include <unistd.h> //testing
-void	print_bits(int to_print)
-{
-	// int bit_location = 7;
-
-	for (int bit_location = 32; bit_location >= 0; bit_location--)
-	{	
-		//this shifts the 1 to 'line up' with the bit to evaluate and the & 'looks' at the bits to see if they are the same or not
-		if (to_print & (1 << bit_location))
-			write(1, "1", 1);
-		else
-			write(1, "0", 1);
-	}
-	write(1, "\n", 1);
-}
-
 int main( void ) {
-	// Fixed a;
-	// Fixed b( a );
-	// Fixed c;
-	// c = b;
-	
-	// std::cout << a.getRawBits() << std::endl;
-	// std::cout << b.getRawBits() << std::endl;
-	// std::cout << c.getRawBits() << std::endl;
 
-	float test = 16.1;
+	Fixed a;
+	Fixed const b( 10 ); 
+	Fixed const c( 42.42f ); 
+	Fixed const d( b );
+	a = Fixed( 1234.4321f );
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 
-	// std::cout << sizeof(int);
-	std::cout << "Test before shift: " << test << std::endl;
-	std::cout << "Test bits before shift: " << std::endl;
-	print_bits(test);
-	// int	test_int = test << 8;
-	int test_int = test * (1 << 8);
-	std::cout <<"Test after shift: " << test_int << std::endl;
-	std::cout << "Test bits after shift: " << std::endl;
-	print_bits(test_int);
 	return 0; 
 }
