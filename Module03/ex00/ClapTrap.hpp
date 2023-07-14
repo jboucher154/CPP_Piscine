9328782900e09094d509d03391f2a7a2b90ec83b
Jenny Boucher <jebouche@c3r4p5.hive.fi>
2023-07-14 13:10:48 +0300

    add initial files for mod03 ex00

---
+++ Module03/ex00/ClapTrap.hpp
@@ -0,0 +1,30 @@
+
+#ifndef CLAP_TRAP_HPP
+# define CLAP_TRAP_HPP
+
+# include <string>
+# include <iostream>
+
+class ClapTrap
+{
+	public:
+		ClapTrap( const ClapTrap& to_copy);
+		ClapTrap( std::string name );
+		~ClapTrap( void );
+
+		ClapTrap&	operator = ( const ClapTrap& to_copy );
+
+		void	attack( const std::string& target );
+		void	takeDamage( unsigned int amount );
+		void	beRepaired( unsigned int amount ); 
+
+	private:
+		ClapTrap( void );
+
+		std::string		_name;
+		int	_hit_points;
+		int	_energy_points;
+		int	_attack_damage;
+};
+
+#endif
---
+++ Module03/ex00/main.cpp
@@ -0,0 +1,36 @@
+
+#include "ClapTrap.hpp"
+
+int	main( void ) {
+
+	// ClapTrap	test; //inaccessable by design
+
+	ClapTrap test = ClapTrap("Clarence");
+	ClapTrap test2 = ClapTrap("Victor");
+	ClapTrap test3 = ClapTrap(test2);
+	ClapTrap test4 = test;
+
+	std::cout << std::endl;
+
+	for (int i = 0; i < 12; i++)
+		test.attack("box");
+	test.beRepaired(10);
+	test.takeDamage(2);
+
+	std::cout << std::endl;
+
+	for (int i = 0; i < 10; i++)
+		test2.takeDamage(2);
+	test2.beRepaired(10);
+	test2.attack("triangle");
+
+	std::cout << std::endl;
+	for (int i = 0; i < 21; i++) {
+		test3.takeDamage(1);
+		test3.beRepaired(2);
+		test3.attack("chair");
+	}
+
+	std::cout << std::endl;
+	return (0);
+}
