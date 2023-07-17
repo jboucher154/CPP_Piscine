2503c94e403176655c71503d95a49e4552bb6163
Jenny Boucher <jebouche@c3r4p5.hive.fi>
2023-07-17 10:27:52 +0300

    more include to header

--- Module01/ex01/Zombie.cpp
+++ Module01/ex01/Zombie.cpp
@@ -6,12 +6,11 @@
 /*   By: jebouche <jebouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 15:44:58 by jebouche          #+#    #+#             */
-/*   Updated: 2023/07/05 16:03:00 by jebouche         ###   ########.fr       */
+/*   Updated: 2023/07/17 10:26:49 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
-#include <iostream>
 
 Zombie::Zombie( void ) {
 	std::cout << "Zombie created" << std::endl;
--- Module01/ex01/Zombie.hpp
+++ Module01/ex01/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: jebouche <jebouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 15:45:02 by jebouche          #+#    #+#             */
-/*   Updated: 2023/07/05 16:00:12 by jebouche         ###   ########.fr       */
+/*   Updated: 2023/07/17 10:26:45 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define ZOMBIE_HPP
 
 # include <string>
+# include <iostream>
 
 class Zombie {
 	public:
