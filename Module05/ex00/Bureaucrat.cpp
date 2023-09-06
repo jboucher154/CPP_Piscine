4a78f588e53c237c4299fa45cf47cdeeaecd7c19
Jenny Boucher <jebouche@c2r4p5.hive.fi>
2023-09-06 12:54:47 +0300

    add inital files for mod 5 ex00

---
+++ Module05/ex00/Bureaucrat.cpp
@@ -0,0 +1,33 @@
+
+#include "Bureaucrat.hpp"
+
+ 
+/* CONSTRUCTORS */
+Bureaucrat::Bureaucrat( void ) {
+	/* default constructor */
+}
+	
+Bureaucrat::Bureaucrat( /*add parameters*/ ) {
+	/* parameterized constructor */
+}
+	
+Bureaucrat::Bureaucrat( const Bureaucrat& to_copy ) {
+	/* copy constructor */
+}
+	
+ 
+/* DESTRUCTOR */
+Bureaucrat::~Bureaucrat( void ) {
+	/* destructor */
+}
+	
+ 
+/* OPERATOR OVERLOADS */
+Bureaucrat&	Bureaucrat::operator=( const Bureaucrat& to_copy ) {
+	/* copy assignment operator overload */
+}
+	
+
+/* CLASS PUBLIC METHODS */
+
+/* CLASS PRIVATE METHODS */
