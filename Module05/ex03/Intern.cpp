42ccc4e0743c8648eadcee939af9fb2b28efefb2
jen <jboucher154@gmail.com>
2023-09-12 09:22:11 +0300

    add intial files for intern class

---
+++ Module05/ex03/Intern.cpp
@@ -0,0 +1,33 @@
+
+#include "Intern.hpp"
+
+ 
+/* CONSTRUCTORS */
+Intern::Intern( void ) {
+	/* default constructor */
+}
+	
+Intern::Intern( /*add parameters*/ ) {
+	/* parameterized constructor */
+}
+	
+Intern::Intern( const Intern& to_copy ) {
+	/* copy constructor */
+}
+	
+ 
+/* DESTRUCTOR */
+Intern::~Intern( void ) {
+	/* destructor */
+}
+	
+ 
+/* OPERATOR OVERLOADS */
+Intern&	Intern::operator=( const Intern& to_copy ) {
+	/* copy assignment operator overload */
+}
+	
+
+/* CLASS PUBLIC METHODS */
+
+/* CLASS PRIVATE METHODS */
