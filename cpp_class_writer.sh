6e1ddba946669d5872880c703c87337065695236
Jenny Boucher <jebouche@c2r4p3.hive.fi>
2023-09-13 17:48:20 +0300

    adjust formating of classes

--- cpp_class_writer.sh
+++ cpp_class_writer.sh
@@ -10,6 +10,9 @@ fi
 CAPS_CLASS_NAME=$(echo "$1" | tr '[:lower:]' '[:upper:]')
 CLASS_FORMAT="class	$1 
 {
+	private:
+		/* PRIVATE METHODS AND MEMBERS */
+
 	public:
 		$1( void );
 		$1( /*add parameters*/ );
@@ -20,9 +23,7 @@ CLASS_FORMAT="class	$1
 		$1&	operator=( const $1& to_copy );
 
 		/* PUBLIC METHODS */
-		
-	private:
-		/* PRIVATE METHODS AND MEMBERS */
+
 };"
 
 HEADER_GUARD_WITH_CLASS="#ifndef ${CAPS_CLASS_NAME}_HPP
@@ -36,38 +37,50 @@ INCLUDES="
 #include \"$1.hpp\"
 
 "
-DEFAULT_CONSTRUCTOR="$1::$1( void ) {
+DEFAULT_CONSTRUCTOR="$1::$1( void )
+{
 	/* default constructor */
 }
-	
 "
-PARAMETERIZED_CONSTRUCTOR="$1::$1( /*add parameters*/ ) {
+
+PARAMETERIZED_CONSTRUCTOR="$1::$1( /*add parameters*/ )
+{
 	/* parameterized constructor */
 }
-	
 "
-COPY_CONSTRUCTOR="$1::$1( const $1& to_copy ) {
+
+COPY_CONSTRUCTOR="$1::$1( const $1& to_copy )
+{
 	/* copy constructor */
 }
-	
 "
-DESTRUCTOR="$1::~$1( void ) {
+
+DESTRUCTOR="$1::~$1( void )
+{
 	/* destructor */
 }
-	
 "
-COPY_ASSIGNMENT="$1&	$1::operator=( const $1& to_copy ) {
+
+COPY_ASSIGNMENT="$1&	$1::operator=( const $1& to_copy )
+{
 	/* copy assignment operator overload */
 }
-	
 "
+
 SOURCE_FORMAT="${INCLUDES} 
+
 /* CONSTRUCTORS */
+
 ${DEFAULT_CONSTRUCTOR}${PARAMETERIZED_CONSTRUCTOR}${COPY_CONSTRUCTOR} 
+
 /* DESTRUCTOR */
+
 ${DESTRUCTOR} 
+
 /* OPERATOR OVERLOADS */
+
 ${COPY_ASSIGNMENT}
+
 /* CLASS PUBLIC METHODS */
 
 /* CLASS PRIVATE METHODS */"
