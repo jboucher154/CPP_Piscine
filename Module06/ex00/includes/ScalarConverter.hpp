b2ed510dca0a5f2667a64bdf08a1836558e923b4
Jenny Boucher <jebouche@c2r5p2.hive.fi>
2023-09-15 18:09:37 +0300

    add intial detect type

--- Module06/ex00/includes/ScalarConverter.hpp
+++ Module06/ex00/includes/ScalarConverter.hpp
@@ -1,22 +1,41 @@
 #ifndef SCALARCONVERTER_HPP
 # define SCALARCONVERTER_HPP
 
+# include <string>
+# include <limits.h>
+# include <float.h>
+
 class	ScalarConverter 
 {
 	private:
-		/* PRIVATE METHODS AND MEMBERS */
-
-	public:
 		ScalarConverter( void );
-		ScalarConverter( /*add parameters*/ );
 		ScalarConverter( const ScalarConverter& to_copy );
-
 		~ScalarConverter( void );
-
 		ScalarConverter&	operator=( const ScalarConverter& to_copy );
 
-		/* PUBLIC METHODS */
+		char	toChar( std::string input ) const;
+		int		toInt( std::string input ) const;
+		float	toFloat( std::string input ) const;
+		double	toDouble( std::string input ) const;
 
+		static int		Type_;
+		static char		char_convert_;
+		static int		int_convert_;
+		static float	float_convert_;
+		static double	double_convert_;
+
+	public:
+		static enum eDataTypes
+		{
+			noDataType,
+			charDataType,
+			intDataType,
+			floatDataType,
+			doubleDataType
+		};
+
+		static void	Convert( std::string input );
+		static void	DetectType( std::string input );
 };
 
 #endif
--- Module06/ex00/srcs/ScalarConverter.cpp
+++ Module06/ex00/srcs/ScalarConverter.cpp
@@ -1,40 +1,90 @@
 
 #include "ScalarConverter.hpp"
 
- 
+int		ScalarConverter::Type_ = ScalarConverter::eDataTypes::noDataType;
+char	ScalarConverter::char_convert_ = 0;
+int		ScalarConverter::int_convert_ = 0;
+float	ScalarConverter::float_convert_ = 0;
+double	ScalarConverter::double_convert_ = 0;
 
 /* CONSTRUCTORS */
 
 ScalarConverter::ScalarConverter( void )
-{
-	/* default constructor */
-}
-ScalarConverter::ScalarConverter( /*add parameters*/ )
-{
-	/* parameterized constructor */
-}
+{/* default constructor */ }
+
 ScalarConverter::ScalarConverter( const ScalarConverter& to_copy )
-{
-	/* copy constructor */
+{ 
+	*this = to_copy;
 }
- 
 
 /* DESTRUCTOR */
-
 ScalarConverter::~ScalarConverter( void )
-{
-	/* destructor */
-}
- 
+{ /* destructor */ }
 
 /* OPERATOR OVERLOADS */
 
 ScalarConverter&	ScalarConverter::operator=( const ScalarConverter& to_copy )
 {
-	/* copy assignment operator overload */
+	( void ) to_copy;
+	return (*this);
 }
 
+/* MEMBER FUNCTIONS */
 
-/* CLASS PUBLIC METHODS */
+char	ScalarConverter::toChar( std::string input ) const
+{
 
-/* CLASS PRIVATE METHODS */
+}
+int	ScalarConverter::toInt( std::string input ) const
+{
+	
+}
+
+float	ScalarConverter::toFloat( std::string input ) const
+{
+	
+}
+
+double	ScalarConverter::toDouble( std::string input ) const
+{
+
+}
+
+/* CLASS STATIC METHODS */
+
+void	ScalarConverter::Convert( std::string input ) 
+{
+
+}
+
+void	ScalarConverter::DetectType( std::string input )
+{
+	//check against empty string, nans, infs...
+	if (input.length() == 1 && !std::isdigit(input[0]))
+	{
+		ScalarConverter::Type_ = ScalarConverter::eDataTypes::charDataType;
+		ScalarConverter::char_convert_ = input.front();
+	}
+	else if (input.back() == 'f') //not account for possible trailing ws
+	{
+		ScalarConverter::Type_ = ScalarConverter::eDataTypes::floatDataType;
+		ScalarConverter::float_convert_ = stof(input, nullptr);
+	}
+	else
+	{
+		long int	longConversion = stol(input, nullptr, 10);
+		long double	doubleConversion = stold(input, nullptr);
+		if (longConversion <= INT_MAX && longConversion >= INT_MIN)
+		{
+			ScalarConverter::Type_ = ScalarConverter::eDataTypes::intDataType;
+		}
+		else if (doubleConversion <= DBL_MAX && doubleConversion <= DBL_MIN)
+		{
+			ScalarConverter::Type_ = ScalarConverter::eDataTypes::doubleDataType;
+		}
+		else
+		{
+			ScalarConverter::Type_ = ScalarConverter::eDataTypes::noDataType;
+		}
+	}
+}
